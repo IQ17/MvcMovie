d8dadf0a7a9912a484ab206ed425bc250a0414bf
iouiwc <lukewang25@163.com>
2015-05-12 22:04:23 +0800

    add init project

---
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            // The URL is similar to a command line,
+            // where controller is the command and action, name, id and so on are parameters.
+            // The syntax of the command line is given by the routes.MapRoute
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
+        }
+    }
+}
